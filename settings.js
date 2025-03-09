e15e63b29d630d3e5b84383b2bdfb90415f6f8be
Yashmika1 <ysathru1@gmail.com>
2025-03-09 22:50:02 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,10 +43,10 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&cKUWEbxR#fPeZRjIbkfpxIXZkVxMpkxgrT4YFNMoeOrAKAX1t9j4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&QC8R0Qia#04zWVVBSj_2jF744JGEAGJO9Rtelbs9PbDL8AYeoEQo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
-SUDO: process.env.SUDO === undefined ? '94702572157' : process.env.SUDO,
+SUDO: process.env.SUDO === undefined ? '94778742627' : process.env.SUDO,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
